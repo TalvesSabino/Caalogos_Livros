18491747eee6256971194815b79f55af81293483
TalvesSabino <talvessabino01@gmail.com>
2019-05-06 12:28:28 -0300

    Teste

---
+++ app/src/main/java/com/example/caalogos_livros/Cadastrar_CL.java
@@ -0,0 +1,13 @@
+package com.example.caalogos_livros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastrar_CL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar__cl);
+    }
+}
